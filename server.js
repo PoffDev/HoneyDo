44d21e7162de3edd7f56fabd77bada1c6914b033
PoffDev <joshuapoffenberger@gmail.com>
2016-07-24 14:38:14 -0400

    handlebars, css, and server completely linked

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var path = require('path');
+var bodyparser = require('body-parser');
 
 //Port
 var app = express();
 var PORT = process.env.PORT || 8081;
+
+// access to the public folder
+app.use(express.static('app/public'));
+
+// bodyparser allows our server to interpret data
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.text());
+app.use(bodyparser.json({type: 'application/vnd.api+json'}));
 
 //Handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
