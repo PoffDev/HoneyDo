fe19a5235d6bfccaf2efc5848006c48bb5f6f556
PoffDev <joshuapoffenberger@gmail.com>
2016-07-27 16:15:51 -0400

    update gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-gulp.task('styles', function(){
+gulp.task('sass', function(){
 	gulp.src('app/public/sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pip(gulp.dest('app/public/css'))
+		.pipe(gulp.dest('app/public/css'));
 });
 
 gulp.task('watch', function(){
