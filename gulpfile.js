cadf958571fc84b899b46d5fae5300e3dd1ca015
PoffDev <joshuapoffenberger@gmail.com>
2016-07-28 19:52:32 -0400

    updated sass, still not working

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var browserSync = require('browser-sync').create();
 gulp.task('sass', function(){
 	gulp.src('app/public/sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('app/public/css'));
+		.pipe(gulp.dest('../app/public/css'));
 });
 
 gulp.task('watch', function(){
