dd902d61de464fc851d483ef6ae87e1b63fbde34
PoffDev <joshuapoffenberger@gmail.com>
2016-08-15 20:35:01 -0400

    added model folder

---
+++ model/user.js
@@ -0,0 +1,18 @@
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var orm = require('../config/orm.js');
+
+function Users (Users) {
+ 	this.userName = userObj.userName
+	this.password = userObj.password
+}
+ //check these names//
+module.exports = Users
+
+// module.exports.saveUser = function(Users, callback){
+// 	orm.addUserToDB(Users, function(status, err){
+// 		if (err) return callback(false);
+// 		callback(true);
+// 	});
+// } 
