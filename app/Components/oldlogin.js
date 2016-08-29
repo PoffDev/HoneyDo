e3e15fba22b599ed46e8c325d32a53affaffeff1
PoffDev <joshuapoffenberger@gmail.com>
2016-08-29 17:39:09 -0400

    old login form

---
+++ app/Components/oldlogin.js
@@ -0,0 +1,51 @@
+<div className = "container text-center">
+
+	            	<div className = "row">
+
+		            	<div className = "col-md-4 col-md-offset-4">
+
+			                <div className="panel panel-default">
+								
+								<div className="panel-heading">
+			    					<h3 className="panel-title">Log in to HoneyDo</h3>
+			  					</div>
+
+			  			
+				  				<div className = "row">
+				  					<div className = "col-md-10 col-md-offset-1">
+				  					<div className="panel-body">
+				    					<div className="form-group">
+				    						<label>Email address</label><br />
+				    						<input type="email"  id="emailInput" className="form-control"/>
+
+										</div>
+									</div>
+									</div>
+								</div>
+
+								<div className = "row">
+				  					<div className = "col-md-10 col-md-offset-1">
+				  					<div className="panel-body">
+				    					<div className="form-group">
+				    						<label>Password</label><br />
+				    						<input type="email"  id="emailInput" className="form-control"/>
+
+										</div>
+									</div>
+									</div>
+								</div>
+
+								<div className = "row">
+
+											<Link to="/dash"><button type="button" className="btn btn-default">Submit</button></Link>
+								</div>
+						
+			  				</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
