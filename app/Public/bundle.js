1c0de955907cf12c5fd6045f4cb895f40a575830
PoffDev <joshuapoffenberger@gmail.com>
2016-10-24 20:00:22 -0400

    updated webpack

--- app/Public/bundle.js
+++ app/Public/bundle.js
@@ -34,7 +34,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "./App/public";
+/******/ 	__webpack_require__.p = "./App/Public";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
